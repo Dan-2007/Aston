50e366bf0c035097c90cfb824354e98a07c54f41
Dan-2007 <zenrocks@yandex.ru>
2025-08-02 14:57:57 +0500

    made refactoring

--- src/main/java/org/example/task1_2/Main.java
+++ src/main/java/org/example/task1_2/Main.java
@@ -3,16 +3,15 @@ package org.example.task1_2;
 import org.example.task1_2.instances.Truck;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         Truck truck = new Truck("FreightLiner Columbia");
-            Field fieldName = truck
+        Field fieldName = truck
                 .getClass()
                 .getSuperclass()
                 .getDeclaredField("name");
-            fieldName.setAccessible(true);
+        fieldName.setAccessible(true);
         System.out.println("Truck's make 'n' model: " + fieldName.get(truck));
         truck.move();
         truck.carryFreight();
